08376456bf34d01a8c240077e686d10f10776523
AndEnd-eafit <aprivasm@eafit.edu.co>
2025-05-15 19:30:25 -0500

    Update app.py

--- app.py
+++ app.py
@@ -17,6 +17,9 @@ st.markdown(
     Debido a políticas de seguridad, el panel no puede mostrarse aquí directamente.
     [Haz clic aquí para abrir el panel en una nueva pestaña.](https://aprivasm.grafana.net/d/74f0fd71-032c-4b23-98aa-d01297b5cf1a/f549e81?orgId=1&from=2025-05-15T23:36:58.635Z&to=2025-05-16T00:06:58.635Z&timezone=browser&viewPanel=panel-4)
     
+     ### Ver panel de Temperatura vs Humedad
+    Debido a políticas de seguridad, el panel no puede mostrarse aquí directamente.
+    [Haz clic aquí para abrir el panel en una nueva pestaña.](https://aprivasm.grafana.net/d/74f0fd71-032c-4b23-98aa-d01297b5cf1a/f549e81?orgId=1&from=2025-05-15T23:36:58.635Z&to=2025-05-16T00:06:58.635Z&timezone=browser&viewPanel=panel-2)
     
    """,
     unsafe_allow_html=True
