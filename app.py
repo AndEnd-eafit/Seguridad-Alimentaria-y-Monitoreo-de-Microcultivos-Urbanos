ff231aa7b62974a479050252ffbc90984e5648cf
AndEnd-eafit <aprivasm@eafit.edu.co>
2025-05-15 19:16:58 -0500

    Create app.py

---
+++ app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.title("Dashboard IoT del Clima")
+
+st.markdown(
+    """
+    ### Ver panel de clima
+    Debido a políticas de seguridad, el panel no puede mostrarse aquí directamente.
+    [Haz clic aquí para abrir el panel en una nueva pestaña.](https://aprivasm.grafana.net/d/74f0fd71-032c-4b23-98aa-d01297b5cf1a/f549e81?orgId=1&from=2025-05-15T23:36:58.635Z&to=2025-05-16T00:06:58.635Z&timezone=browser&viewPanel=panel-6)
+    """,
+    unsafe_allow_html=True
+)
