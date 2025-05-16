1df66ab46cdb180552794e0aac3ef431782a2843
AndEnd-eafit <aprivasm@eafit.edu.co>
2025-05-15 19:28:14 -0500

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ st.markdown(
     [Haz clic aquí para abrir el panel en una nueva pestaña.](https://aprivasm.grafana.net/d/74f0fd71-032c-4b23-98aa-d01297b5cf1a/f549e81?orgId=1&from=2025-05-15T23:36:58.635Z&to=2025-05-16T00:06:58.635Z&timezone=browser&viewPanel=panel-6)
     
 
-    ## Ver panel de Humedad
+    ### Ver panel de Humedad
     Debido a políticas de seguridad, el panel no puede mostrarse aquí directamente.
     [Haz clic aquí para abrir el panel en una nueva pestaña.](https://aprivasm.grafana.net/d/74f0fd71-032c-4b23-98aa-d01297b5cf1a/f549e81?orgId=1&from=2025-05-15T23:36:58.635Z&to=2025-05-16T00:06:58.635Z&timezone=browser&viewPanel=panel-3)
     
