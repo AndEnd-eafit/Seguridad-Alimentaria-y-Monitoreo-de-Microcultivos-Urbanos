22d73e497938f8f32167391a5a1192d6228229c9
AndEnd-eafit <aprivasm@eafit.edu.co>
2025-05-15 19:23:16 -0500

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,6 @@ st.markdown(
     ### Ver panel de clima
     Debido a políticas de seguridad, el panel no puede mostrarse aquí directamente.
     [Haz clic aquí para abrir el panel en una nueva pestaña.](https://aprivasm.grafana.net/d/74f0fd71-032c-4b23-98aa-d01297b5cf1a/f549e81?orgId=1&from=2025-05-15T23:36:58.635Z&to=2025-05-16T00:06:58.635Z&timezone=browser&viewPanel=panel-6)
-   (https://aprivasm.grafana.net/d/74f0fd71-032c-4b23-98aa-d01297b5cf1a/f549e81?orgId=1&from=2025-05-15T23:36:58.635Z&to=2025-05-16T00:06:58.635Z&timezone=browser&viewPanel=panel-3)(https://aprivasm.grafana.net/d/74f0fd71-032c-4b23-98aa-d01297b5cf1a/f549e81?orgId=1&from=2025-05-15T23:36:58.635Z&to=2025-05-16T00:06:58.635Z&timezone=browser&viewPanel=panel-4)(https://aprivasm.grafana.net/d/74f0fd71-032c-4b23-98aa-d01297b5cf1a/f549e81?orgId=1&from=2025-05-15T23:36:58.635Z&to=2025-05-16T00:06:58.635Z&timezone=browser&viewPanel=panel-2)(https://aprivasm.grafana.net/d/74f0fd71-032c-4b23-98aa-d01297b5cf1a/f549e81?orgId=1&from=2025-05-15T23:36:58.635Z&to=2025-05-16T00:06:58.635Z&timezone=browser&viewPanel=panel-5)(https://aprivasm.grafana.net/d/74f0fd71-032c-4b23-98aa-d01297b5cf1a/f549e81?orgId=1&from=2025-05-15T23:36:58.635Z&to=2025-05-16T00:06:58.635Z&timezone=browser&viewPanel=panel-1) """,
+   """,
     unsafe_allow_html=True
 )
